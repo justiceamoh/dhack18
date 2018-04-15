6d780563902eec7571ab2335b4240a9779bcee92
Justice Amoh <justiceamoh@gmail.com>
2018-04-15 01:48:27 -0400

    Revise doc2vec to be more effective

--- doc2vec.py
+++ doc2vec.py
@@ -4,6 +4,10 @@ from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from nltk import RegexpTokenizer
 from nltk.corpus import stopwords
 import pandas as pd
+import multiprocessing
+
+cores = multiprocessing.cpu_count()
+assert gensim.models.doc2vec.FAST_VERSION > -1, "This will be painfully slow otherwise"
 
 
 fname = 'yelp.json'
@@ -15,43 +19,37 @@ gc.collect()
 
 
 tokenizer = RegexpTokenizer(r'\w+')
-stopword_set = set(stopwords.words('english'))
+stopset   = set(stopwords.words('english'))
 
-def nlp_clean(data):
-   new_data = []
-   for i in range(len(data)):
-      new_str = data[i].lower()
-      dlist = tokenizer.tokenize(new_str)
-      dlist = list(set(dlist).difference(stopword_set))
-      new_data.append(dlist)
-   return new_data
+docs      = []
 
+for i in range(len(data)):
+    tag    = label[i]
+    words  = data[i].lower()
+    tokens = tokenizer.tokenize(words)
+    tokens = [w for w in tokens if not w in stopset]
+    docs.append(TaggedDocument(tokens,tag))
 
-class TaggedDocs(object):
-    def __init__(self, doc_list, labels_list):
-        self.labels_list = labels_list
-        self.doc_list = doc_list
-    def __iter__(self):
-        for idx, doc in enumerate(self.doc_list):
-              yield TaggedDocument(doc, [self.labels_list[idx]])
-
-dat = nlp_clean(data)
-itr = TaggedDocs(data,label)
 
 ndim  = 100
-model = Doc2Vec(vector_size=ndim, window=10, min_count=1, 
-                alpha=0.025, min_alpha=0.025, workers=2,dm=1)
-model.build_vocab(it)
+model = Doc2Vec(dm=0,vector_size=ndim, window=10, min_count=3,
+                alpha=0.025, min_alpha=0.025, workers=cores)
+model.build_vocab(docs)
+
 
 # Training of model
-model.train(it, total_examples=len(dat), epochs=2)
+for epoch in range(10):
+    print 'iteration '+str(epoch+1)
+    model.train(docs, total_examples=len(docs), epochs=1)
+    model.alpha -= 0.001
+    model.min_alpha = model.alpha
 
 
 # Saving the created model
-# model.save('doc2vec.model')
-# print 'model saved'
+model.save('doc2vec100.model')
+print 'model saved'
 
-model = Doc2Vec.load('doc2vec.model')
-print 'model loaded'
+# model = Doc2Vec.load('doc2vec.model')
+# print 'model loaded'
 
 # docvec = model.docvecs.infer_vector('war.txt')
